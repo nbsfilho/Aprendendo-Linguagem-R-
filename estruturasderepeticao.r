8ef3d92ae399922d85f47156abb83347e52bcf2f
Nilton Barbosa Santos Filho <71855454+nbsfilho@users.noreply.github.com>
2023-02-21 21:01:48 -0300

    Create estruturasderepeticao.r

---
+++ estruturasderepeticao.r
@@ -0,0 +1,32 @@
+#Estrutururas de repetição
+
+#Loops
+
+#For
+
+# for (valor in sequencia){
+#   código...
+# }
+
+?seq
+
+#Exemplo for
+for (i in seq(12)){
+  print(i)
+}
+
+
+#While
+
+# while(condição){
+#   código...
+# }
+
+
+#Exemplo while
+
+i <- 0
+while(i <= 10){
+  print(i)
+  i = i+1
+}
